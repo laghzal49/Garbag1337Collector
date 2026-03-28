c4fc2c2dcba75eca4515ad1b3675e0570bc9beb2
tlaghzal <laghzaltarik0@gmail.com>
2026-03-28 06:04:51 +0100

    Norm 42

--- ft_malloc.h
+++ ft_malloc.h
@@ -6,7 +6,7 @@
 /*   By: tlaghzal <tlaghzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/28 06:00:00 by tlaghzal          #+#    #+#             */
-/*   Updated: 2026/03/28 06:02:20 by tlaghzal         ###   ########.fr       */
+/*   Updated: 2026/03/28 06:04:36 by tlaghzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,17 @@
 #define FT_MALLOC_H
 
 #include <stdlib.h>
+
 typedef struct s_list_gc
 {
 	struct s_list_gc	*prev;
 	struct s_list_gc	*next;
 }	t_list_gc;
 
-void ft_panic(void *ptr);
-void free_all(void);
-void *ft_malloc(size_t size);
-t_list_gc **get_manger(void);
-void ft_free(void *ptr);
+void	ft_panic(void *ptr);
+void	free_all(void);
+void	*ft_malloc(size_t size);
+t_list_gc	**get_manger(void);
+void	ft_free(void *ptr);
+
 #endif
