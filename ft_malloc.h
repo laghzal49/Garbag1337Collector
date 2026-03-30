176d1e5981bec377f84166f071e6ee65ae0f126c
tlaghzal <laghzaltarik0@gmail.com>
2026-03-30 10:15:20 +0100

    feat: initialize 1337 Garbage Collector project with ft_malloc.h header definitions
    Establish custom memory allocator and garbage collector project structure in C.
    
    - Created ft_malloc.h declaring custom malloc, free, and collector interfaces.
    - Defined t_block chunk metadata structure tracking allocated heap regions.

---
+++ ft_malloc.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_malloc.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlaghzal <tlaghzal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/28 06:00:00 by tlaghzal          #+#    #+#             */
+/*   Updated: 2026/03/28 06:16:20 by tlaghzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_MALLOC_H
+# define FT_MALLOC_H
+
+# include <stdlib.h>
+# include <pthread.h>
+
+typedef struct s_list_gc
+{
+	struct s_list_gc	*prev;
+	struct s_list_gc	*next;
+	void				*pad1;
+	void				*pad2;
+}	t_list_gc;
+
+void		free_all(void);
+void		*ft_malloc(size_t size);
+t_list_gc	**get_manager(void);
+void		ft_free(void *ptr);
+
+#endif
